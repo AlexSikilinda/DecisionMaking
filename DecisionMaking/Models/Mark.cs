fb5885994a488ccbac566e2551942221940bdd98
Alexander Sikilinda <asikilinda@madisonpg.com>
2016-05-31 19:16:10 +0300

    add mark properties

--- DecisionMaking/Models/Mark.cs
+++ DecisionMaking/Models/Mark.cs
@@ -7,5 +7,16 @@ namespace DecisionMaking.Models
 {
     public class Mark
     {
+        public int MNum { get; set; }
+
+        public int CNum { get; set; }
+
+        public string MName { get; set; }
+
+        public int MRange { get; set; }
+
+        public int NumMark { get; set; }
+
+        public int NormMark { get; set; }
     }
 }
